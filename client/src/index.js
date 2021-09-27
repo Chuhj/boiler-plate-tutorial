c225bcf376f3417fb8ec5cdedb4f6a661ad893e9
Chuhj <chj9574@naver.com>
2021-09-28 02:56:12 +0900

    create store, apply redux

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import promiseMiddleware from 'redux-promise';
+import rootReducer from './_reducers/index';
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(promiseMiddleware, thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
