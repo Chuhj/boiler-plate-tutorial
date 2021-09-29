d7919112017f82ba38bb2dcf4b5a7c245105ba2a
Chuhj <chj9574@naver.com>
2021-09-29 23:50:10 +0900

    feat:  Implement register page

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,14 +1,26 @@
 import axios from 'axios';
-import { LOGIN_USER } from './types';
+import { LOGIN_USER, REGISTER_USER } from './types';
 
 export function loginUser(data) {
   const response = axios
     .post('/api/users/login', data)
     .then((res) => res.data)
     .catch((err) => err);
-  console.log(response);
+
   return {
     type: LOGIN_USER,
+    payload: response,
+  };
+}
+
+export function registerUser(data) {
+  const response = axios
+    .post('/api/users/register', data)
+    .then((res) => res.data)
+    .catch((err) => err);
+
+  return {
+    type: REGISTER_USER,
     payload: response,
   };
 }
--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,7 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
-function RegisterPage() {
-  return <div>RegisterPage</div>;
+import { registerUser } from '../../../_actions/user_action';
+
+function RegisterPage(props) {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmsetPassword] = useState('');
+
+  const dispatch = useDispatch();
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleChangeName = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleChangeConfirmPassword = (e) => {
+    setConfirmsetPassword(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      return alert('비밀번호 다름');
+    }
+    const data = { name, email, password };
+
+    dispatch(registerUser(data)).then((response) => {
+      if (response.payload.success) {
+        props.history.push('/login');
+      } else {
+        alert('Register Failed!');
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        height: '100vh',
+        alignItems: 'center',
+      }}
+    >
+      <form
+        onSubmit={handleFormSubmit}
+        style={{ display: 'flex', flexDirection: 'column' }}
+      >
+        <label>Email</label>
+        <input type="text" onChange={handleChangeEmail} required />
+        <label>Name</label>
+        <input type="text" onChange={handleChangeName} required />
+        <label>Password</label>
+        <input
+          type="password"
+          onChange={handleChangePassword}
+          minLength="5"
+          required
+        />
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          onChange={handleChangeConfirmPassword}
+          minLength="5"
+          required
+        />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 }
 
 export default RegisterPage;
